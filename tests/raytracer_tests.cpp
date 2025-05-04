1c601bfe3a7857030ab5395f00aee250ff263733
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-04 20:41:18 +0200

    test overloaded arithmetic operators

--- tests/raytracer_tests.cpp
+++ tests/raytracer_tests.cpp
@@ -2,11 +2,6 @@
 #include <boost/math/special_functions/next.hpp>
 #include "../include/maths.hpp"
 
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
-}
-
 TEST(FPTests, ApproximatelyEqualEpsilonAddSmall) {
     float a = 0.0;
     float b = 0.0;
@@ -39,6 +34,17 @@ TEST(PointTests, CreatePoint) {
     EXPECT_EQ(point.w(), 1);
 }
 
+TEST(PointTests, ChangePoint) {
+    raytracer::maths::Point3D point(1, 2, 3);
+    point.x() = 2;
+    point.y() = 4;
+    point.z() = 8;
+    EXPECT_EQ(point.x(), 2);
+    EXPECT_EQ(point.y(), 4);
+    EXPECT_EQ(point.z(), 8);
+    EXPECT_EQ(point.w(), 1);
+}
+
 
 TEST(VectorTests, CreateVector) {
     raytracer::maths::Vector3D vector(0, 0, 0);
@@ -46,4 +52,82 @@ TEST(VectorTests, CreateVector) {
     EXPECT_EQ(vector.y(), 0);
     EXPECT_EQ(vector.z(), 0);
     EXPECT_EQ(vector.w(), 0);
+}
+
+TEST(VectorTests, ChangeVector) {
+    raytracer::maths::Vector3D vector(1, 2, 3);
+    vector.x() = 2;
+    vector.y() = 4;
+    vector.z() = 8;
+    EXPECT_EQ(vector.x(), 2);
+    EXPECT_EQ(vector.y(), 4);
+    EXPECT_EQ(vector.z(), 8);
+    EXPECT_EQ(vector.w(), 0);
+}
+
+TEST(VectorTests, AddTwoVectors) {
+    raytracer::maths::Vector3D v1(1, 2, 3);
+    raytracer::maths::Vector3D v2(2, 4, 4);
+    auto result = v1 + v2;
+    EXPECT_EQ(result.x(), 3);
+    EXPECT_EQ(result.y(), 6);
+    EXPECT_EQ(result.z(), 7);
+    EXPECT_EQ(result.w(), 0);
+}
+
+TEST(VectorTests, SubTwoVectors) {
+    raytracer::maths::Vector3D v1(1, 2, 3);
+    raytracer::maths::Vector3D v2(2, 4, 4);
+    auto result = v1 - v2;
+    EXPECT_EQ(result.x(), -1);
+    EXPECT_EQ(result.y(), -2);
+    EXPECT_EQ(result.z(), -1);
+    EXPECT_EQ(result.w(), 0);
+}
+
+TEST(VectorTests, LeftMultByScalar) {
+    raytracer::maths::Vector3D v(1, 2, 3);
+    auto result = 2 * v;
+    EXPECT_EQ(result.x(), 2);
+    EXPECT_EQ(result.y(), 4);
+    EXPECT_EQ(result.z(), 6);
+    EXPECT_EQ(result.w(), 0);
+}
+
+TEST(VectorTests, RightMultByScalar) {
+    raytracer::maths::Vector3D v(1, 2, 3);
+    auto result = v * 2;
+    EXPECT_EQ(result.x(), 2);
+    EXPECT_EQ(result.y(), 4);
+    EXPECT_EQ(result.z(), 6);
+    EXPECT_EQ(result.w(), 0);
+}
+
+TEST(VectorTests, CompoundAddTwoVectors) {
+    raytracer::maths::Vector3D v1(1, 2, 3);
+    raytracer::maths::Vector3D v2(2, 4, 4);
+    v1 += v2;
+    EXPECT_EQ(v1.x(), 3);
+    EXPECT_EQ(v1.y(), 6);
+    EXPECT_EQ(v1.z(), 7);
+    EXPECT_EQ(v1.w(), 0);
+}
+
+TEST(VectorTests, CompoundSubTwoVectors) {
+    raytracer::maths::Vector3D v1(1, 2, 3);
+    raytracer::maths::Vector3D v2(2, 4, 4);
+    v1 -= v2;
+    EXPECT_EQ(v1.x(), -1);
+    EXPECT_EQ(v1.y(), -2);
+    EXPECT_EQ(v1.z(), -1);
+    EXPECT_EQ(v1.w(), 0);
+}
+
+TEST(VectorTests, CompoundMultVectorScalar) {
+    raytracer::maths::Vector3D v(1, 2, 3);
+    v *= 2;
+    EXPECT_EQ(v.x(), 2);
+    EXPECT_EQ(v.y(), 4);
+    EXPECT_EQ(v.z(), 6);
+    EXPECT_EQ(v.w(), 0);
 }
