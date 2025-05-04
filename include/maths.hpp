f443cfd7ebb2055a9117bafb1edc745d3d79fb89
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-04 20:14:12 +0200

    added overloaded getter methods that return a reference for use on non-const objects

--- include/maths.hpp
+++ include/maths.hpp
@@ -15,6 +15,10 @@ namespace raytracer::maths {
         T y() const { return y_; }
         T z() const { return z_; }
         T w() const { return w_; }
+        T& x() { return x_; }
+        T& y() { return y_; }
+        T& z() { return z_; }
+        T& w() { return w_; }
         friend std::ostream& operator<<(std::ostream& os, const Point3D<T>& p) {
             os << "(" << p.x_ << ", " << p.y_ << ", " << p.z_ << ")";
             return os;
@@ -31,6 +35,10 @@ namespace raytracer::maths {
         T y() const { return y_; }
         T z() const { return z_; }
         T w() const { return w_; }
+        T& x() { return x_; }
+        T& y() { return y_; }
+        T& z() { return z_; }
+        T& w() { return w_; }
         friend std::ostream& operator<<(std::ostream& os, const Vector3D<T>& v) {
             os << "[" << v.x_ << ", " << v.y_ << ", " << v.z_ << "]";
             return os;
