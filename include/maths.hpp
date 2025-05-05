8e6237e83fe25f924ca3705484e89e1f402a9c9c
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-05 16:26:16 +0200

    more operator overloading and unit tests

--- include/maths.hpp
+++ include/maths.hpp
@@ -5,31 +5,6 @@
 
 namespace raytracer::maths {
     template<typename T>
-    class Point3D {
-    private:
-        T x_, y_, z_, w_;
-
-    public:
-        Point3D(T x, T y, T z) : x_(x), y_(y), z_(z), w_(1) {
-        }
-
-        T x() const { return x_; }
-        T y() const { return y_; }
-        T z() const { return z_; }
-        T w() const { return w_; }
-
-        T &x() { return x_; }
-        T &y() { return y_; }
-        T &z() { return z_; }
-        T &w() { return w_; }
-
-        friend std::ostream &operator<<(std::ostream &os, const Point3D<T> &p) {
-            os << "(" << p.x_ << ", " << p.y_ << ", " << p.z_ << ")";
-            return os;
-        }
-    };
-
-    template<typename T>
     class Vector3D {
     private:
         T x_, y_, z_, w_;
@@ -69,6 +44,10 @@ namespace raytracer::maths {
             return Vector3D<T>(scalar * rhs.x_, scalar * rhs.y_, scalar * rhs.z_);
         }
 
+        friend Vector3D<T> operator/(const Vector3D<T> &lhs, T scalar) {
+            return Vector3D<T>(lhs.x_ / scalar, lhs.y_ / scalar, lhs.z_ / scalar);
+        }
+
         Vector3D<T> &operator+=(const Vector3D<T> &rhs) {
             x_ += rhs.x_;
             y_ += rhs.y_;
@@ -89,10 +68,60 @@ namespace raytracer::maths {
             z_ *= scalar;
             return *this;
         }
+
+        Vector3D<T> &operator-() {
+            x_ = -x_;
+            y_ = -y_;
+            z_ = -z_;
+            return *this;
+        }
     };
 
+    template<typename T>
+    class Point3D {
+    private:
+        T x_, y_, z_, w_;
+
+    public:
+        Point3D(T x, T y, T z) : x_(x), y_(y), z_(z), w_(1) {
+        }
+
+        T x() const { return x_; }
+        T y() const { return y_; }
+        T z() const { return z_; }
+        T w() const { return w_; }
+
+        T &x() { return x_; }
+        T &y() { return y_; }
+        T &z() { return z_; }
+        T &w() { return w_; }
+
+        friend std::ostream &operator<<(std::ostream &os, const Point3D<T> &p) {
+            os << "(" << p.x_ << ", " << p.y_ << ", " << p.z_ << ")";
+            return os;
+        }
+
+        friend Point3D<T> operator+(const Point3D<T> &lhs, Vector3D<T>& rhs) {
+            return Point3D<T>(lhs.x_ + rhs.x(), lhs.y_ + rhs.y(), lhs.z_ + rhs.z());
+        }
+
+        friend Point3D<T> operator+(const Vector3D<T> &lhs, Point3D<T>& rhs) {
+            return Point3D(rhs.x_ + lhs.x(), rhs.y_ + lhs.y(), rhs.z_ + lhs.z());
+        }
+
+        friend Vector3D<T> operator-(const Point3D<T> &lhs, Point3D<T>& rhs) {
+            return Vector3D<T>(lhs.x_ - rhs.x_, lhs.y_ - rhs.y_, lhs.z_ - rhs.z_);
+        }
+
+        friend Point3D<T> operator-(const Point3D<T> &lhs, Vector3D<T>& rhs) {
+            return Point3D<T>(lhs.x_ - rhs.x(), lhs.y_ - rhs.y(), lhs.z_ - rhs.z());
+        }
+    };
+
+
     template<std::floating_point T>
-    bool AlmostEqualEpsilon(T a, T b, T tolerance = std::numeric_limits<T>::epsilon()) {
+    bool AlmostEqualEpsilon(T a, T b,
+                            T tolerance = std::numeric_limits<T>::epsilon()) {
         return std::abs(a - b) < tolerance;
     }
 }
