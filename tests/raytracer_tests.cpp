a26fab5af5eb22bce61321092400abe282a70150
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-07 21:36:21 +0200

    unit tests for cross and dot products, removed test with integer template type

--- tests/raytracer_tests.cpp
+++ tests/raytracer_tests.cpp
@@ -27,163 +27,163 @@ TEST(FPTests, ApproximatelyEqualEpsilonMultiplySmall) {
 }
 
 TEST(PointTests, CreatePoint) {
-    raytracer::maths::Point3D point(0, 0, 0);
-    EXPECT_EQ(point.x(), 0);
-    EXPECT_EQ(point.y(), 0);
-    EXPECT_EQ(point.z(), 0);
-    EXPECT_EQ(point.w(), 1);
+    raytracer::maths::Point3D point(0.0, 0.0, 0.0);
+    EXPECT_NEAR(point.x(), 0.0, 1e-6);
+    EXPECT_NEAR(point.y(), 0.0, 1e-6);
+    EXPECT_NEAR(point.z(), 0.0, 1e-6);
+    EXPECT_NEAR(point.w(), 1.0, 1e-6);
 }
 
 TEST(PointTests, MovePointWithRightVectorAdd) {
-    raytracer::maths::Vector3D v(1, 2, 3);
-    raytracer::maths::Point3D p(1, 2, 3);
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
+    raytracer::maths::Point3D p(1.0, 2.0, 3.0);
 
     auto new_p = p + v;
 
-    EXPECT_EQ(new_p.x(), 2);
-    EXPECT_EQ(new_p.y(), 4);
-    EXPECT_EQ(new_p.z(), 6);
-    EXPECT_EQ(new_p.w(), 1);
+    EXPECT_NEAR(new_p.x(), 2.0, 1e-6);
+    EXPECT_NEAR(new_p.y(), 4.0, 1e-6);
+    EXPECT_NEAR(new_p.z(), 6.0, 1e-6);
+    EXPECT_NEAR(new_p.w(), 1.0, 1e-6);
 }
 
 TEST(PointTests, MovePointWithLeftVectorAdd) {
-    raytracer::maths::Vector3D v(1, 2, 3);
-    raytracer::maths::Point3D p(1, 2, 3);
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
+    raytracer::maths::Point3D p(1.0, 2.0, 3.0);
 
     auto new_p = v + p;
 
-    EXPECT_EQ(new_p.x(), 2);
-    EXPECT_EQ(new_p.y(), 4);
-    EXPECT_EQ(new_p.z(), 6);
-    EXPECT_EQ(new_p.w(), 1);
+    EXPECT_NEAR(new_p.x(), 2.0, 1e-6);
+    EXPECT_NEAR(new_p.y(), 4.0, 1e-6);
+    EXPECT_NEAR(new_p.z(), 6.0, 1e-6);
+    EXPECT_NEAR(new_p.w(), 1.0, 1e-6);
 }
 
 TEST(PointTests, MovePointBackward) {
-    raytracer::maths::Vector3D v(5, 6, 7);
-    raytracer::maths::Point3D p(3, 2, 1);
+    raytracer::maths::Vector3D v(5.0, 6.0, 7.0);
+    raytracer::maths::Point3D p(3.0, 2.0, 1.0);
     auto new_p = p - v;
 
-    EXPECT_EQ(new_p.x(), -2);
-    EXPECT_EQ(new_p.y(), -4);
-    EXPECT_EQ(new_p.z(), -6);
-    EXPECT_EQ(new_p.w(), 1);
+    EXPECT_NEAR(new_p.x(), -2.0, 1e-6);
+    EXPECT_NEAR(new_p.y(), -4.0, 1e-6);
+    EXPECT_NEAR(new_p.z(), -6.0, 1e-6);
+    EXPECT_NEAR(new_p.w(), 1.0, 1e-6);
 }
 
 TEST(PointTests, ChangePoint) {
-    raytracer::maths::Point3D point(1, 2, 3);
-    point.x() = 2;
-    point.y() = 4;
-    point.z() = 8;
-    EXPECT_EQ(point.x(), 2);
-    EXPECT_EQ(point.y(), 4);
-    EXPECT_EQ(point.z(), 8);
-    EXPECT_EQ(point.w(), 1);
+    raytracer::maths::Point3D point(1.0, 2.0, 3.0);
+    point.x() = 2.0;
+    point.y() = 4.0;
+    point.z() = 8.0;
+    EXPECT_NEAR(point.x(), 2.0, 1e-6);
+    EXPECT_NEAR(point.y(), 4.0, 1e-6);
+    EXPECT_NEAR(point.z(), 8.0, 1e-6);
+    EXPECT_NEAR(point.w(), 1.0, 1e-6);
 }
 
 TEST(VectorTests, CreateVector) {
-    raytracer::maths::Vector3D vector(0, 0, 0);
-    EXPECT_EQ(vector.x(), 0);
-    EXPECT_EQ(vector.y(), 0);
-    EXPECT_EQ(vector.z(), 0);
-    EXPECT_EQ(vector.w(), 0);
+    raytracer::maths::Vector3D vector(0.0, 0.0, 0.0);
+    EXPECT_NEAR(vector.x(), 0.0, 1e-6);
+    EXPECT_NEAR(vector.y(), 0.0, 1e-6);
+    EXPECT_NEAR(vector.z(), 0.0, 1e-6);
+    EXPECT_NEAR(vector.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, ChangeVector) {
-    raytracer::maths::Vector3D vector(1, 2, 3);
-    vector.x() = 2;
-    vector.y() = 4;
-    vector.z() = 8;
-    EXPECT_EQ(vector.x(), 2);
-    EXPECT_EQ(vector.y(), 4);
-    EXPECT_EQ(vector.z(), 8);
-    EXPECT_EQ(vector.w(), 0);
+    raytracer::maths::Vector3D vector(1.0, 2.0, 3.0);
+    vector.x() = 2.0;
+    vector.y() = 4.0;
+    vector.z() = 8.0;
+    EXPECT_NEAR(vector.x(), 2.0, 1e-6);
+    EXPECT_NEAR(vector.y(), 4.0, 1e-6);
+    EXPECT_NEAR(vector.z(), 8.0, 1e-6);
+    EXPECT_NEAR(vector.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, AddTwoVectors) {
-    raytracer::maths::Vector3D v1(1, 2, 3);
-    raytracer::maths::Vector3D v2(2, 4, 4);
+    raytracer::maths::Vector3D v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D v2(2.0, 4.0, 4.0);
     auto result = v1 + v2;
-    EXPECT_EQ(result.x(), 3);
-    EXPECT_EQ(result.y(), 6);
-    EXPECT_EQ(result.z(), 7);
-    EXPECT_EQ(result.w(), 0);
+    EXPECT_NEAR(result.x(), 3.0, 1e-6);
+    EXPECT_NEAR(result.y(), 6.0, 1e-6);
+    EXPECT_NEAR(result.z(), 7.0, 1e-6);
+    EXPECT_NEAR(result.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, SubTwoVectors) {
-    raytracer::maths::Vector3D v1(1, 2, 3);
-    raytracer::maths::Vector3D v2(2, 4, 4);
+    raytracer::maths::Vector3D v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D v2(2.0, 4.0, 4.0);
     auto result = v1 - v2;
-    EXPECT_EQ(result.x(), -1);
-    EXPECT_EQ(result.y(), -2);
-    EXPECT_EQ(result.z(), -1);
-    EXPECT_EQ(result.w(), 0);
+    EXPECT_NEAR(result.x(), -1.0, 1e-6);
+    EXPECT_NEAR(result.y(), -2.0, 1e-6);
+    EXPECT_NEAR(result.z(), -1.0, 1e-6);
+    EXPECT_NEAR(result.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, LeftMultByScalar) {
-    raytracer::maths::Vector3D v(1, 2, 3);
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
     auto result = 2 * v;
-    EXPECT_EQ(result.x(), 2);
-    EXPECT_EQ(result.y(), 4);
-    EXPECT_EQ(result.z(), 6);
-    EXPECT_EQ(result.w(), 0);
+    EXPECT_NEAR(result.x(), 2.0, 1e-6);
+    EXPECT_NEAR(result.y(), 4.0, 1e-6);
+    EXPECT_NEAR(result.z(), 6.0, 1e-6);
+    EXPECT_NEAR(result.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, RightMultByScalar) {
-    raytracer::maths::Vector3D v(1, 2, 3);
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
     auto result = v * 2;
-    EXPECT_EQ(result.x(), 2);
-    EXPECT_EQ(result.y(), 4);
-    EXPECT_EQ(result.z(), 6);
-    EXPECT_EQ(result.w(), 0);
+    EXPECT_NEAR(result.x(), 2.0, 1e-6);
+    EXPECT_NEAR(result.y(), 4.0, 1e-6);
+    EXPECT_NEAR(result.z(), 6.0, 1e-6);
+    EXPECT_NEAR(result.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, CompoundAddTwoVectors) {
-    raytracer::maths::Vector3D v1(1, 2, 3);
-    raytracer::maths::Vector3D v2(2, 4, 4);
+    raytracer::maths::Vector3D v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D v2(2.0, 4.0, 4.0);
     v1 += v2;
-    EXPECT_EQ(v1.x(), 3);
-    EXPECT_EQ(v1.y(), 6);
-    EXPECT_EQ(v1.z(), 7);
-    EXPECT_EQ(v1.w(), 0);
+    EXPECT_NEAR(v1.x(), 3.0, 1e-6);
+    EXPECT_NEAR(v1.y(), 6.0, 1e-6);
+    EXPECT_NEAR(v1.z(), 7.0, 1e-6);
+    EXPECT_NEAR(v1.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, CompoundSubTwoVectors) {
-    raytracer::maths::Vector3D v1(1, 2, 3);
-    raytracer::maths::Vector3D v2(2, 4, 4);
+    raytracer::maths::Vector3D v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D v2(2.0, 4.0, 4.0);
     v1 -= v2;
-    EXPECT_EQ(v1.x(), -1);
-    EXPECT_EQ(v1.y(), -2);
-    EXPECT_EQ(v1.z(), -1);
-    EXPECT_EQ(v1.w(), 0);
+    EXPECT_NEAR(v1.x(), -1.0, 1e-6);
+    EXPECT_NEAR(v1.y(), -2.0, 1e-6);
+    EXPECT_NEAR(v1.z(), -1.0, 1e-6);
+    EXPECT_NEAR(v1.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, CompoundMultVectorScalar) {
-    raytracer::maths::Vector3D v(1, 2, 3);
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
     v *= 2;
-    EXPECT_EQ(v.x(), 2);
-    EXPECT_EQ(v.y(), 4);
-    EXPECT_EQ(v.z(), 6);
-    EXPECT_EQ(v.w(), 0);
+    EXPECT_NEAR(v.x(), 2.0, 1e-6);
+    EXPECT_NEAR(v.y(), 4.0, 1e-6);
+    EXPECT_NEAR(v.z(), 6.0, 1e-6);
+    EXPECT_NEAR(v.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, GetVectorFromPointSubtraction) {
-    raytracer::maths::Point3D v1(3, 2, 1);
-    raytracer::maths::Point3D v2(5, 6, 7);
+    raytracer::maths::Point3D v1(3.0, 2.0, 1.0);
+    raytracer::maths::Point3D v2(5.0, 6.0, 7.0);
     auto result = v1 - v2;
 
-    EXPECT_EQ(result.x(), -2);
-    EXPECT_EQ(result.y(), -4);
-    EXPECT_EQ(result.z(), -6);
-    EXPECT_EQ(result.w(), 0);
+    EXPECT_NEAR(result.x(), -2.0, 1e-6);
+    EXPECT_NEAR(result.y(), -4.0, 1e-6);
+    EXPECT_NEAR(result.z(), -6.0, 1e-6);
+    EXPECT_NEAR(result.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, NegateVector) {
-    raytracer::maths::Vector3D v(1, -2, 1);
+    raytracer::maths::Vector3D v(1.0, -2.0, 1.0);
     -v;
-    EXPECT_EQ(v.x(), -1);
-    EXPECT_EQ(v.y(), 2);
-    EXPECT_EQ(v.z(), -1);
-    EXPECT_EQ(v.w(), 0);
+    EXPECT_NEAR(v.x(), -1.0, 1e-6);
+    EXPECT_NEAR(v.y(), 2.0, 1e-6);
+    EXPECT_NEAR(v.z(), -1.0, 1e-6);
+    EXPECT_NEAR(v.w(), 0.0, 1e-6);
 }
 
 TEST(VectorTests, RightDivByScalar) {
@@ -196,69 +196,69 @@ TEST(VectorTests, RightDivByScalar) {
 }
 
 TEST(VectorTests, VectorLengthFloat) {
-    raytracer::maths::Vector3D v(1, 0, 0);
-    auto length = v.length<float>();
+    raytracer::maths::Vector3D<float> v(1.0, 0.0, 0.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthDouble) {
-    raytracer::maths::Vector3D v(1, 0, 0);
-    auto length = v.length<double>();
+    raytracer::maths::Vector3D v(1.0, 0.0, 0.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthFloat2) {
-    raytracer::maths::Vector3D v(0, 1, 0);
-    auto length = v.length<float>();
+    raytracer::maths::Vector3D<float> v(0.0, 1.0, 0.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthDouble2) {
-    raytracer::maths::Vector3D v(0, 1, 0);
-    auto length = v.length<double>();
+    raytracer::maths::Vector3D v(0.0, 1.0, 0.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthFloat3) {
-    raytracer::maths::Vector3D v(0, 0, 1);
-    auto length = v.length<float>();
+    raytracer::maths::Vector3D<float> v(0.0, 0.0, 1.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthDouble3) {
-    raytracer::maths::Vector3D v(0, 0, 1);
-    auto length = v.length<double>();
+    raytracer::maths::Vector3D v(0.0, 0.0, 1.0);
+    auto length = v.length();
     EXPECT_NEAR(length, 1.0, 1e-6);
 }
 
 TEST(VectorTests, VectorLengthFloat4) {
-    raytracer::maths::Vector3D v(1, 2, 3);
-    auto length = v.length<float>();
+    raytracer::maths::Vector3D<float> v(1.0, 2.0, 3.0);
+    auto length = v.length();
     EXPECT_NEAR(length, std::sqrt(14), 1e-6);
 }
 
 TEST(VectorTests, VectorLengthDouble4) {
-    raytracer::maths::Vector3D v(1, 2, 3);
-    auto length = v.length<double>();
+    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
+    auto length = v.length();
     EXPECT_NEAR(length, std::sqrt(14), 1e-6);
 }
 
 TEST(VectorTests, VectorLengthFloat5) {
-    raytracer::maths::Vector3D v(-1, -2, -3);
-    auto length = v.length<float>();
+    raytracer::maths::Vector3D<float> v(-1.0, -2.0, -3.0);
+    auto length = v.length();
     EXPECT_NEAR(length, std::sqrt(14), 1e-6);
 }
 
 TEST(VectorTests, VectorLengthDouble5) {
-    raytracer::maths::Vector3D v(-1, -2, -3);
-    auto length = v.length<double>();
+    raytracer::maths::Vector3D v(-1.0, -2.0, -3.0);
+    auto length = v.length();
     EXPECT_NEAR(length, std::sqrt(14), 1e-6);
 }
 
 TEST(VectorTests, NormalizeVectorWithMemberFunctionFloat) {
-    raytracer::maths::Vector3D v(4, 0, 0);
-    v.normalize<float>();
-    EXPECT_NEAR(v.length<float>(), 1.0, 1e-6);
+    raytracer::maths::Vector3D<float> v(4.0, 0.0, 0.0);
+    v.normalize();
+    EXPECT_NEAR(v.length(), 1.0, 1e-6);
     EXPECT_NEAR(v.x(), 1.0, 1e-6);
     EXPECT_NEAR(v.y(), 0.0, 1e-6);
     EXPECT_NEAR(v.z(), 0.0, 1e-6);
@@ -266,9 +266,9 @@ TEST(VectorTests, NormalizeVectorWithMemberFunctionFloat) {
 }
 
 TEST(VectorTests, NormalizeVectorWithMemberFunctionDouble) {
-    raytracer::maths::Vector3D v(4, 0, 0);
-    v.normalize<double>();
-    EXPECT_NEAR(v.length<double>(), 1.0, 1e-6);
+    raytracer::maths::Vector3D v(4.0, 0.0, 0.0);
+    v.normalize();
+    EXPECT_NEAR(v.length(), 1.0, 1e-6);
     EXPECT_NEAR(v.x(), 1.0, 1e-6);
     EXPECT_NEAR(v.y(), 0.0, 1e-6);
     EXPECT_NEAR(v.z(), 0.0, 1e-6);
@@ -276,9 +276,9 @@ TEST(VectorTests, NormalizeVectorWithMemberFunctionDouble) {
 }
 
 TEST(VectorTests, NormalizeVectorWithMemberFunctionFloat2) {
-    raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
-    v.normalize<float>();
-    EXPECT_NEAR(v.length<float>(), 1.0, 1e-6);
+    raytracer::maths::Vector3D<float> v(1.0, 2.0, 3.0);
+    v.normalize();
+    EXPECT_NEAR(v.length(), 1.0, 1e-6);
     EXPECT_NEAR(v.x(), (1.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(v.y(), (2.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(v.z(), (3.0 / std::sqrt(14)), 1e-6);
@@ -287,8 +287,8 @@ TEST(VectorTests, NormalizeVectorWithMemberFunctionFloat2) {
 
 TEST(VectorTests, NormalizeVectorWithMemberFunctionDouble2) {
     raytracer::maths::Vector3D v(1.0, 2.0, 3.0);
-    v.normalize<double>();
-    EXPECT_NEAR(v.length<double>(), 1.0, 1e-6);
+    v.normalize();
+    EXPECT_NEAR(v.length(), 1.0, 1e-6);
     EXPECT_NEAR(v.x(), (1.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(v.y(), (2.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(v.z(), (3.0 / std::sqrt(14)), 1e-6);
@@ -297,8 +297,8 @@ TEST(VectorTests, NormalizeVectorWithMemberFunctionDouble2) {
 
 TEST(VectorTests, NormalizeVectorWithFreeFunctionFloat) {
     raytracer::maths::Vector3D<float> v(1.0, 2.0, 3.0);
-    auto result = normalize<float>(v);
-    EXPECT_NEAR(result.length<float>(), 1.0, 1e-6);
+    auto result = normalize(v);
+    EXPECT_NEAR(result.length(), 1.0, 1e-6);
     EXPECT_NEAR(result.x(), (1.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(result.y(), (2.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(result.z(), (3.0 / std::sqrt(14)), 1e-6);
@@ -307,10 +307,57 @@ TEST(VectorTests, NormalizeVectorWithFreeFunctionFloat) {
 
 TEST(VectorTests, NormalizeVectorWithFreeFunctionDouble2) {
     raytracer::maths::Vector3D<double> v(1.0, 2.0, 3.0);
-    auto result = normalize<double>(v);
-    EXPECT_NEAR(result.length<double>(), 1.0, 1e-6);
+    auto result = normalize(v);
+    EXPECT_NEAR(result.length(), 1.0, 1e-6);
     EXPECT_NEAR(result.x(), (1.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(result.y(), (2.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(result.z(), (3.0 / std::sqrt(14)), 1e-6);
     EXPECT_NEAR(result.w(), 0.0, 1e-6);
+}
+
+TEST(VectorTests, VectorDotProductFloat) {
+    raytracer::maths::Vector3D<float> v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D<float> v2(2.0, 3.0, 4.0);
+
+    float dot_prod = v1.dot(v2);
+    EXPECT_NEAR(dot_prod, 20.0, 1e-6);
+
+    dot_prod = dot<float>(v1, v2);
+    EXPECT_NEAR(dot_prod, 20.0, 1e-6);
+}
+
+TEST(VectorTests, VectorDotProductDouble) {
+    raytracer::maths::Vector3D<double> v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D<double> v2(2.0, 3.0, 4.0);
+
+    double dot_prod = v1.dot(v2);
+    EXPECT_NEAR(dot_prod, 20.0, 1e-6);
+
+    dot_prod = dot<double>(v1, v2);
+    EXPECT_NEAR(dot_prod, 20.0, 1e-6);
+}
+
+TEST(VectorTests, VectorCrossProductFloat) {
+    raytracer::maths::Vector3D<float> v1(1.0, 2.0, 3.0);
+    raytracer::maths::Vector3D<float> v2(2.0, 3.0, 4.0);
+
+    auto cross_prod_v1v2 = v1.cross(v2);
+    EXPECT_NEAR(cross_prod_v1v2.x(), -1.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v1v2.y(), 2.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v1v2.z(), -1.0, 1e-6);
+
+    auto cross_prod_v2v1 = v2.cross(v1);
+    EXPECT_NEAR(cross_prod_v2v1.x(), 1.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v2v1.y(), -2.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v2v1.z(), 1.0, 1e-6);
+
+    cross_prod_v1v2 = cross<float>(v1, v2);
+    EXPECT_NEAR(cross_prod_v1v2.x(), -1.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v1v2.y(), 2.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v1v2.z(), -1.0, 1e-6);
+
+    cross_prod_v2v1 = cross<double>(v2, v1);
+    EXPECT_NEAR(cross_prod_v2v1.x(), 1.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v2v1.y(), -2.0, 1e-6);
+    EXPECT_NEAR(cross_prod_v2v1.z(), 1.0, 1e-6);
 }
