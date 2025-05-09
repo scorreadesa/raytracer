0bdb3d7142a347d4fba62a0cdb14c254828495ac
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-09 15:34:56 +0200

    define custom exceptions for row/col access out of range

---
+++ include/exceptions.h
@@ -0,0 +1,24 @@
+//
+// Created by antares on 09.05.25.
+//
+
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+#include <stdexcept>
+
+
+namespace raytracer::exceptions {
+    class ColumnOutOfRangeException : public std::out_of_range {
+    public:
+        explicit ColumnOutOfRangeException(const std::string &message) : std::out_of_range(message) {
+        };
+    };
+
+    class RowOutOfRangeException : public std::out_of_range {
+    public:
+        explicit RowOutOfRangeException(const std::string &message) : std::out_of_range(message) {
+        };
+    };
+}
+
+#endif //EXCEPTIONS_H
