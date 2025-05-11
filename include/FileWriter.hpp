222207df7a09ccf2caf8afa3332adb11fc635ec6
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-11 12:03:09 +0200

    replaced Canvas typename with T to avoid confusions

--- include/FileWriter.hpp
+++ include/FileWriter.hpp
@@ -3,18 +3,18 @@
 
 #include "IFileWriter.hpp"
 
-template<typename Canvas>
+template<typename T>
 class FileWriter {
 public:
-    explicit FileWriter(std::unique_ptr<IFileWriter<Canvas> > writer) : writer_(std::move(writer)) {
+    explicit FileWriter(std::unique_ptr<IFileWriter<T> > writer) : writer_(std::move(writer)) {
     }
 
-    void write(const Canvas &canvas, const std::filesystem::path &path, const std::string &file) {
-        writer_->write(canvas, path, file);
+    void write(const T &data, const std::filesystem::path &path, const std::string &file) {
+        writer_->write(data, path, file);
     }
 
 private:
-    std::unique_ptr<IFileWriter<Canvas> > writer_;
+    std::unique_ptr<IFileWriter<T> > writer_;
 };
 
 #endif //FILEWRITER_H
