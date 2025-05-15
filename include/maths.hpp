9ddc83d008ff4d1fc749144a20d0f22d629b5dcf
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-05-15 21:18:41 +0200

    implemented helper functions that convert vectors to matrices and back

--- include/maths.hpp
+++ include/maths.hpp
@@ -383,14 +383,21 @@ namespace raytracer::maths {
     };
 
     template<std::floating_point T>
-    Matrix<T> to_column_matrix(const Point3D<T> &point) {
+    Matrix<T> point_to_column_matrix(const Point3D<T> &point) {
         return Matrix<T>(4, 1, {
                              point.x(), point.y(), point.z(), point.w()
                          });
     }
 
     template<std::floating_point T>
-    Point3D<T> from_column_matrix(const Matrix<T> &matrix) {
+    Matrix<T> vector_to_column_matrix(const Vector3D<T> &v) {
+        return Matrix<T>(4, 1, {
+            v.x(), v.y(), v.z(), v.w()
+        });
+    }
+
+    template<std::floating_point T>
+    Point3D<T> point_from_column_matrix(const Matrix<T> &matrix) {
         return Point3D<T>(
             matrix(0, 0) / matrix(3, 0),
             matrix(1, 0) / matrix(3, 0),
@@ -399,18 +406,43 @@ namespace raytracer::maths {
     }
 
     template<std::floating_point T>
-    Matrix<T> to_row_matrix(const Point3D<T> &point) {
+    Vector3D<T> vector_from_column_matrix(const Matrix<T> &matrix) {
+        return Vector3D<T>(
+            matrix(0, 0),
+            matrix(1, 0),
+            matrix(2, 0)
+        );
+    }
+
+    template<std::floating_point T>
+    Matrix<T> point_to_row_matrix(const Point3D<T> &point) {
         return Matrix<T>(1, 4, {
             point.x(), point.y(), point.z(), point.w()
         });
     }
 
     template<std::floating_point T>
-    Point3D<T> from_row_matrix(const Matrix<T>& matrix) {
+    Matrix<T> vector_to_row_matrix(const Vector3D<T> &v) {
+        return Matrix<T>(1, 4, {
+            v.x(), v.y(), v.z(), v.w()
+        });
+    }
+
+    template<std::floating_point T>
+    Point3D<T> point_from_row_matrix(const Matrix<T>& matrix) {
         return Point3D<T>(
             matrix(0, 0) / matrix(0, 3),
             matrix(0, 1) / matrix(0, 3),
             matrix(0, 2) / matrix(0, 3)
+        );
+    }
+
+    template<std::floating_point T>
+    Vector3D<T> vector_from_row_matrix(const Matrix<T>& matrix) {
+        return Vector3D<T>(
+            matrix(0, 0),
+            matrix(0, 1),
+            matrix(0, 2)
         );
     }
 
