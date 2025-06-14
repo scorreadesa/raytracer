ba73f72d253c42741486a499c13513916a3f5571
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-06-14 18:01:57 +0200

    make each member function of the interface const

---
+++ include/Solver.hpp
@@ -0,0 +1,18 @@
+#ifndef SOLVER_HPP
+#define SOLVER_HPP
+
+#include <cmath>
+#include "maths.hpp"
+
+namespace raytracer::maths {
+
+    template<std::floating_point T>
+    class Solver {
+    public:
+        virtual ~Solver() = default;
+        virtual T determinant(const Matrix<T> & m) const = 0;
+        virtual Matrix<T> inverse(const Matrix<T>& m) const = 0;
+    };
+}
+
+#endif //SOLVER_HPP
