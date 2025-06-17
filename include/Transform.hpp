73694a02b2fa7db491a9badfde224a21c3da05f5
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-06-17 23:57:40 +0200

    add scaling tranformation

--- include/Transform.hpp
+++ include/Transform.hpp
@@ -8,7 +8,7 @@ namespace raytracer::maths {
     template <std::floating_point T>
     class Transform4x4 {
     public:
-        static Matrix<T> make_translation(T x, T y, T z) {
+        static Matrix<T> translation(T x, T y, T z) {
             return Matrix<T>(4, 4, {
                 1, 0, 0, x,
                 0, 1, 0, y,
@@ -16,6 +16,15 @@ namespace raytracer::maths {
                 0, 0, 0, 1
             });
         }
+        static Matrix<T> scaling(T x, T y, T z) {
+            return Matrix<T>(4, 4, {
+                x, 0, 0, 0,
+                0, y, 0, 0,
+                0, 0, z, 0,
+                0, 0, 0, 1
+            });
+        }
+
     };
 }
 
