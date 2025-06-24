b6283d0d122f6ef40f42e92105bbd0486f3f9dfc
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-06-24 21:02:03 +0200

    test rotation about the y axis

--- tests/raytracer_tests.cpp
+++ tests/raytracer_tests.cpp
@@ -2141,7 +2141,7 @@ TEST(TransformationsTests, TestRotateXFullQuarter) {
 TEST(TransformationsTests, TestInverseRotateX) {
     const auto point = raytracer::maths::Point3D<double>(0, 1, 0);
     const auto half_quarter = raytracer::maths::Transform4x4<double>::rotation_x(std::numbers::pi / 4.);
-    std::cout << half_quarter << std::endl;
+
     const auto solver = raytracer::maths::CofactorExpansion<double>();
     const auto inverse = solver.inverse(half_quarter);
     const auto reverse_rot = inverse * point;
@@ -2150,4 +2150,28 @@ TEST(TransformationsTests, TestInverseRotateX) {
     EXPECT_NEAR(reverse_rot.y(), std::sqrt(2) / 2., 1e-6);
     EXPECT_NEAR(reverse_rot.z(), -std::sqrt(2) / 2., 1e-6);
     EXPECT_DOUBLE_EQ(reverse_rot.w(), 1);
+}
+
+TEST(TransformationsTests, TestRotateYHalfQuarter) {
+    const auto point = raytracer::maths::Point3D<double>(0, 0, 1);
+    const auto half_quarter = raytracer::maths::Transform4x4<double>::rotation_y(std::numbers::pi / 4.);
+
+    const auto res = half_quarter * point;
+
+    EXPECT_NEAR(res.x(), std::sqrt(2) / 2, 1e-6);
+    EXPECT_NEAR(res.y(), 0, 1e-6);
+    EXPECT_NEAR(res.z(), std::sqrt(2) / 2, 1e-6);
+    EXPECT_DOUBLE_EQ(res.w(), 1);
+}
+
+TEST(TransformationTests, TestRotateYFullQuarter) {
+    const auto point = raytracer::maths::Point3D<double>(0, 0, 1);
+    const auto full_quarter = raytracer::maths::Transform4x4<double>::rotation_y(std::numbers::pi / 2.);
+
+    const auto res = full_quarter * point;
+
+    EXPECT_NEAR(res.x(), 1, 1e-6);
+    EXPECT_NEAR(res.y(), 0, 1e-6);
+    EXPECT_NEAR(res.z(), 0, 1e-6);
+    EXPECT_DOUBLE_EQ(res.w(), 1);
 }
