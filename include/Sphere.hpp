87ec64b0a4ce7ac298e39eb2603b177b07ed6667
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-07-16 12:03:59 +0200

    compute ray-sphere intersection

--- include/Sphere.hpp
+++ include/Sphere.hpp
@@ -15,12 +15,25 @@ namespace raytracer::scene {
         Sphere(const origin& origin, T radius) : origin_(origin), radius_(radius) {}
 
         intersections intersect(const maths::Ray<T>& ray) const override {
-            intersections xs;
+
             auto sphere_to_ray = ray.origin() - origin_;
             auto a = dot(ray.direction(), ray.direction());
             auto b = static_cast<T>(2) * dot(ray.direction(), sphere_to_ray);
             auto c = dot(sphere_to_ray, sphere_to_ray) - static_cast<T>(1);
             auto d = maths::discriminant(a, b, c);
+
+            if (d < 0) {
+                intersections xs;
+                return xs;
+            }
+
+            intersections xs;
+
+            T t1 = (-b - std::sqrt(d)) / (static_cast<T>(2) * a);
+            T t2 = (-b + std::sqrt(d)) / (static_cast<T>(2) * a);
+            xs.emplace_back(Intersection<T>(t1));
+            xs.emplace_back(Intersection<T>(t2));
+
             return xs;
         }
 
