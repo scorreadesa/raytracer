c9f27c923ecb76b23089850e0f93c6fe19d41a36
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-07-17 18:45:33 +0200

    add the intersected shape to the Intersection struct

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -1,16 +1,18 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
 
-#include <cmath>
-
 #include "maths.hpp"
 #include "Ray.hpp"
 
 namespace raytracer::scene {
+    template <std::floating_point T>
+    class Shape;
 
     template <std::floating_point T>
     struct Intersection {
         T t_;
+        const Shape<T>& shape_;
+        Intersection(T t, const Shape<T>& shape) : t_(t), shape_(shape) {}
     };
 
     template <std::floating_point T>
