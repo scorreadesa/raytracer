d2b7935fd5a3ed10714f8a052fcbec4bda5e98fa
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-07-24 00:44:13 +0200

    implement additional constructors

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -5,38 +5,56 @@
 #include "Ray.hpp"
 
 namespace raytracer::scene {
-    template <std::floating_point T>
+    template<std::floating_point T>
     class Shape;
 
-    template <std::floating_point T>
+    template<std::floating_point T>
     struct Intersection {
         T t_;
-        const Shape<T>& shape_;
-        Intersection(T t, const Shape<T>& shape) : t_(t), shape_(shape) {}
+        const Shape<T> &shape_;
 
-        bool operator<(const Intersection<T>& intersection) const {
+        Intersection(T t, const Shape<T> &shape) : t_(t), shape_(shape) {
+        }
+
+        bool operator<(const Intersection<T> &intersection) const {
             return t_ < intersection.t_;
         }
     };
 
-    template <std::floating_point T>
-    bool operator<(const Intersection<T>& lhs, const Intersection<T>& rhs) {
+    template<std::floating_point T>
+    bool operator<(const Intersection<T> &lhs, const Intersection<T> &rhs) {
         return lhs.t_ < rhs.t_;
     }
 
-    template <std::floating_point T>
+    template<std::floating_point T>
     class Shape {
     public:
-        Shape() : transform_(maths::Matrix<T>::identity(4)) {}
-        explicit Shape(const maths::Matrix<T>& transform) : transform_(transform) {}
-        virtual ~Shape() = default;
-        virtual std::vector<Intersection<T>> intersect(const maths::Ray<T>& ray) const = 0;
-        virtual maths::Vector3D<T> normal_at(const maths::Point3D<T>& world_point) const = 0;
+        Shape() : transform_(maths::Matrix<T>::identity(4)), material_(shading::Material<T>()) {
+        }
 
-        maths::Matrix<T> transform() const { return transform_;}
-        shading::Material<T> material() const { return material_;}
-        void set_transform(const maths::Matrix<T>& transform) {transform_ = transform;}
-        void set_material(const shading::Material<T>& material) {material_ = material;}
+        explicit Shape(const maths::Matrix<T> &transform) : transform_(transform), material_(shading::Material<T>()) {
+        }
+
+        Shape(const maths::Matrix<T> &transform, const shading::Material<T> &material) : transform_(transform),
+            material_(material) {
+        }
+
+        explicit Shape(const shading::Material<T> &material) : transform_(maths::Matrix<T>::identity(4)),
+                                                               material_(material) {
+        }
+
+        virtual ~Shape() = default;
+
+        virtual std::vector<Intersection<T> > intersect(const maths::Ray<T> &ray) const = 0;
+
+        virtual maths::Vector3D<T> normal_at(const maths::Point3D<T> &world_point) const = 0;
+
+        maths::Matrix<T> transform() const { return transform_; }
+        shading::Material<T> material() const { return material_; }
+
+        void set_transform(const maths::Matrix<T> &transform) { transform_ = transform; }
+        void set_material(const shading::Material<T> &material) { material_ = material; }
+
     private:
         maths::Matrix<T> transform_;
         shading::Material<T> material_;
