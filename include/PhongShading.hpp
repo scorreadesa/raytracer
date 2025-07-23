52d0af11faa1d3df26fec96cdabcac2cf3f4722a
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-07-24 00:57:05 +0200

    check if the dot product between the light and the normal is strictly smaller than 0

--- include/PhongShading.hpp
+++ include/PhongShading.hpp
@@ -24,7 +24,7 @@ namespace raytracer::shading {
             auto diffuse = drawing::Color<T>(0, 0, 0);
             auto specular = drawing::Color<T>(0, 0, 0);
 
-            if (light_dot_normal <= 0) {
+            if (light_dot_normal < static_cast<T>(0)) {
                 diffuse = drawing::Color<T>(0, 0, 0);
                 specular = drawing::Color<T>(0, 0, 0);
             } else {
@@ -32,7 +32,7 @@ namespace raytracer::shading {
                 const auto reflectv = maths::reflect(-lightv, normal);
                 const auto reflect_dot_eye = dot(reflectv, eye);
 
-                if (reflect_dot_eye <= 0) {
+                if (reflect_dot_eye <= static_cast<T>(0)) {
                     specular = drawing::Color<T>(0.0, 0.0, 0.0);
                 } else {
                     const auto factor = std::pow(reflect_dot_eye, material.shininess_);
