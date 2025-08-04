33fee1f255ae21fe20694603bf51a65aa42a36a0
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-08-04 17:28:44 +0200

    store a pointer to the base Material struct

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -34,17 +34,17 @@ namespace raytracer::scene {
     template<std::floating_point T>
     class Shape {
     public:
-        Shape() : transform_(maths::Matrix<T>::identity(4)), material_(shading::Material<T>()) {
+        Shape() : transform_(maths::Matrix<T>::identity(4)), material_(std::make_shared<shading::PhongMaterial<T>>()) {
         }
 
-        explicit Shape(const maths::Matrix<T> &transform) : transform_(transform), material_(shading::Material<T>()) {
+        explicit Shape(const maths::Matrix<T> &transform) : transform_(transform), material_(std::make_shared<shading::PhongMaterial<T>>()) {
         }
 
-        Shape(const maths::Matrix<T> &transform, const shading::Material<T> &material) : transform_(transform),
+        Shape(const maths::Matrix<T> &transform, const std::shared_ptr<shading::Material<T>> &material) : transform_(transform),
             material_(material) {
         }
 
-        explicit Shape(const shading::Material<T> &material) : transform_(maths::Matrix<T>::identity(4)),
+        explicit Shape(const std::shared_ptr<shading::Material<T>> &material) : transform_(maths::Matrix<T>::identity(4)),
                                                                material_(material) {
         }
 
@@ -57,7 +57,7 @@ namespace raytracer::scene {
         virtual std::unique_ptr<Shape<T>> clone() const = 0;
 
         maths::Matrix<T> transform() const { return transform_; }
-        shading::Material<T> material() const { return material_; }
+        std::shared_ptr<shading::Material<T>> material() const { return material_; }
 
         void set_transform(const maths::Matrix<T> &transform) { transform_ = transform; }
         void set_material(const shading::Material<T> &material) { material_ = material; }
@@ -66,7 +66,7 @@ namespace raytracer::scene {
 
     private:
         maths::Matrix<T> transform_;
-        shading::Material<T> material_;
+        std::shared_ptr<shading::Material<T>> material_;
     };
 }
 
