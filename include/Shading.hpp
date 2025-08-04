1f54588f8380704090decc74c31b9d6a18a95f65
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-08-04 15:33:38 +0200

    introduce ShadingContext struct for shading-related parameters

--- include/Shading.hpp
+++ include/Shading.hpp
@@ -2,10 +2,17 @@
 #define SHADING_HPP
 
 #include "drawing.hpp"
+#include "PhongShading.hpp"
 #include "Point3D.hpp"
 #include "PointLight.hpp"
 
 namespace raytracer::shading {
+    template<std::floating_point T>
+    class Shading;
+
+    template<std::floating_point T>
+    class PhongShading;
+
     template<std::floating_point T>
     struct Material {
         drawing::Color<T> color_;
@@ -13,12 +20,28 @@ namespace raytracer::shading {
         T diffuse_;
         T specular_;
         T shininess_;
+        std::shared_ptr<Shading<T> > shading_;
 
         Material() = default;
 
         Material(const drawing::Color<T> &color, T ambient, T diffuse, T specular, T shininess)
-            : color_(color), ambient_(ambient), diffuse_(diffuse), specular_(specular), shininess_(shininess) {
+            : color_(color), ambient_(ambient), diffuse_(diffuse), specular_(specular), shininess_(shininess),
+              shading_(std::make_shared<PhongShading<T>>()) {
         }
+    };
+
+    template<std::floating_point T>
+    struct ShadingContext {
+        const Material<T>& material_;
+        const Light<T>& light_;
+        maths::Point3D<T> position_;
+        maths::Vector3D<T> eye_;
+        maths::Vector3D<T> normal_;
+
+        ShadingContext() = delete;
+        ShadingContext(const Material<T>& material, const Light<T>& light, const maths::Point3D<T>& position,
+            const maths::Vector3D<T>& eye, const maths::Vector3D<T>& normal) :
+        material_(material), light_(light), position_(position), eye_(eye), normal_(normal) {}
     };
 
     template<std::floating_point T>
@@ -27,11 +50,7 @@ namespace raytracer::shading {
         virtual ~Shading() = default;
 
         virtual drawing::Color<T> shade(
-            const Material<T> &material,
-            const PointLight<T> &point_light,
-            const maths::Point3D<T> &position,
-            const maths::Vector3D<T> &eye,
-            const maths::Vector3D<T> &normal
+            const ShadingContext<T>& context
         ) const = 0;
     };
 }
