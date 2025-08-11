6b0d8c7923caa193db41c0c8f3f36e276f421f7e
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-08-11 17:58:19 +0200

    implement model

--- include/PhongShading.hpp
+++ include/PhongShading.hpp
@@ -9,14 +9,20 @@ namespace raytracer::shading {
     template<std::floating_point T>
     class PhongShading final : public Shading<T> {
     public:
+
+        [[nodiscard]] ShadingModel model() const override {
+            return ShadingModel::Phong;
+        }
+
         drawing::Color<T> shade(
             const ShadingContext<T> &context
         ) const override {
-            const auto *phong_material = dynamic_cast<const PhongMaterial<T> *>(&context.material_);
 
-            const auto effective_color = phong_material->color_ * context.light_.intensity();
+            const auto& phong_material = static_cast<const PhongMaterial<T>&>(context.material_);
+
+            const auto effective_color = phong_material.color_ * context.light_.intensity();
             auto lightv = normalize(context.light_.position() - context.position_);
-            const auto ambient = effective_color * phong_material->ambient_;
+            const auto ambient = effective_color * phong_material.ambient_;
             const auto light_dot_normal = dot(lightv, context.normal_);
 
             auto diffuse = drawing::Color<T>(0, 0, 0);
@@ -26,20 +32,22 @@ namespace raytracer::shading {
                 diffuse = drawing::Color<T>(0, 0, 0);
                 specular = drawing::Color<T>(0, 0, 0);
             } else {
-                diffuse = effective_color * phong_material->diffuse_ * light_dot_normal;
+                diffuse = effective_color * phong_material.diffuse_ * light_dot_normal;
                 const auto reflectv = maths::reflect(-lightv, context.normal_);
                 const auto reflect_dot_eye = dot(reflectv, context.eye_);
 
                 if (reflect_dot_eye <= static_cast<T>(0)) {
                     specular = drawing::Color<T>(0.0, 0.0, 0.0);
                 } else {
-                    const auto factor = std::pow(reflect_dot_eye, phong_material->shininess_);
-                    specular = context.light_.intensity() * phong_material->specular_ * factor;
+                    const auto factor = std::pow(reflect_dot_eye, phong_material.shininess_);
+                    specular = context.light_.intensity() * phong_material.specular_ * factor;
                 }
             }
 
             return ambient + diffuse + specular;
         }
+    private:
+        PhongMaterial<T> phong_material_;
     };
 }
 
