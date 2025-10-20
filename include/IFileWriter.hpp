dfe1ff0b757243be4982b2f67639bc1d0345a60d
Sofiane Correa de Sa <sofiane.correadesa@gmail.com>
2025-10-20 22:16:08 +0200

    delete copy/move operations

--- include/IFileWriter.hpp
+++ include/IFileWriter.hpp
@@ -5,7 +5,14 @@
 template<typename T>
 class IFileWriter {
 public:
+    IFileWriter() = default;
+
     virtual ~IFileWriter() = default;
+
+    IFileWriter(const IFileWriter&) = delete;
+    IFileWriter& operator=(const IFileWriter&) = delete;
+    IFileWriter(IFileWriter&&) = delete;
+    IFileWriter& operator=(IFileWriter&&) = delete;
 
     virtual void write(const T &data, const std::filesystem::path &path, const std::string &filename) const = 0;
 };
